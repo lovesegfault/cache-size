44a9977ca7a3731c4d05e8b557d5bb3b3f9b60ad
Bernardo Meurer <meurerbernardo@gmail.com>
2018-08-13 14:42:47 -0300

    Added docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,19 @@
+//! A library to quickly get the size and line size of your CPU caches.
+//!
+//! Currently this crate only supports x86 CPUs, since it relies on the `CPUID` instruction, via
+//! the [`raw_cpuid`](raw_cpuid) crate. It is a goal to support other architectures; PRs are
+//! welcome!
+//!
+//! Check the [Intel 64 and IA-32 Architectures Software Developers Manual](https://software.intel.com/sites/default/files/managed/39/c5/325462-sdm-vol-1-2abcd-3abcd.pdf)
+//! for more information on the `CPUID` instruction.
 extern crate raw_cpuid;
 
 use raw_cpuid::CpuId;
 
+/// All different types of cache
+///
+/// This is just a thin wrapper over [`raw_cpuid::CacheType`](raw_cpuid::CacheType) listing only the
+/// interesting possible types, and using more user-friendly CameCase names.
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub enum CacheType {
     /// Data cache
@@ -22,6 +34,14 @@ impl Into<raw_cpuid::CacheType> for CacheType {
     }
 }
 
+/// Returns the total size in bytes of `level` cache with type `ctype`.
+///
+/// The only possibilities for this returning `None` are if the system does not support cache
+/// parameters, in which case [`get_cache_parameters()`](raw_cpuid::CpuId::get_cache_parameters) will
+/// fail, or if the selected cache level and/or type does not exist.
+///
+/// This is computed as `associativity * line_size * sets`, and if there are multiple caches
+/// available, it returns the size of the **smallest** cache.
 #[inline]
 pub fn cache_size(level: u8, ctype: CacheType) -> Option<usize> {
     let cpuid = CpuId::new();
@@ -33,6 +53,14 @@ pub fn cache_size(level: u8, ctype: CacheType) -> Option<usize> {
     Some(cache_size)
 }
 
+/// Returns the line size in bytes of `level` cache with type `ctype`.
+///
+/// The only possibilities for this returning `None` are if the system does not support cache
+/// parameters, in which case [`get_cache_parameters()`](raw_cpuid::CpuId::get_cache_parameters) will
+/// fail, or if the selected cache level and/or type does not exist.
+///
+/// This is computed from [`coherency_line_size()`](raw_cpuid::CacheParameter::coherency_line_size),
+/// and if there are multiple caches available, it returns the size of the **smallest** cache.
 #[inline]
 pub fn cache_line_size(level: u8, ctype: CacheType) -> Option<usize> {
     let cpuid = CpuId::new();
@@ -44,31 +72,79 @@ pub fn cache_line_size(level: u8, ctype: CacheType) -> Option<usize> {
     Some(cache_line_size)
 }
 
+/// Returns the total size in bytes of the L1 data cache.
+///
+/// The only possibilities for this returning `None` are if the system does not support cache
+/// parameters, in which case [`get_cache_parameters()`](raw_cpuid::CpuId::get_cache_parameters) will
+/// fail, or if the system has no L1 data cache (if it's a unified L1 cache for example).
+///
+/// This is computed as `associativity * line_size * sets`, and if there are multiple caches
+/// available, it returns the size of the **smallest** cache.
 #[inline]
 pub fn l1_cache_size() -> Option<usize> {
     cache_size(1, CacheType::Data)
 }
 
+/// Returns the line size in bytes of the L1 data cache.
+///
+/// The only possibilities for this returning `None` are if the system does not support cache
+/// parameters, in which case [`get_cache_parameters()`](raw_cpuid::CpuId::get_cache_parameters) will
+/// fail, or if the system has no L1 data cache (if it's a unified L1 cache for example).
+///
+/// This is computed from [`coherency_line_size()`](raw_cpuid::CacheParameter::coherency_line_size),
+/// and if there are multiple caches available, it returns the size of the **smallest** cache.
 #[inline]
 pub fn l1_cache_line_size() -> Option<usize> {
     cache_line_size(1, CacheType::Data)
 }
 
+/// Returns the total size in bytes of the unified L2 cache.
+///
+/// The only possibilities for this returning `None` are if the system does not support cache
+/// parameters, in which case [`get_cache_parameters()`](raw_cpuid::CpuId::get_cache_parameters) will
+/// fail, or if the system has no unified L2 cache.
+///
+/// This is computed as `associativity * line_size * sets`, and if there are multiple caches
+/// available, it returns the size of the **smallest** cache.
 #[inline]
 pub fn l2_cache_size() -> Option<usize> {
     cache_size(2, CacheType::Unified)
 }
 
+/// Returns the line size in bytes of the unified L2 cache.
+///
+/// The only possibilities for this returning `None` are if the system does not support cache
+/// parameters, in which case [`get_cache_parameters()`](raw_cpuid::CpuId::get_cache_parameters) will
+/// fail, or if the system has no unified L2 cache.
+///
+/// This is computed from [`coherency_line_size()`](raw_cpuid::CacheParameter::coherency_line_size),
+/// and if there are multiple caches available, it returns the size of the **smallest** cache.
 #[inline]
 pub fn l2_cache_line_size() -> Option<usize> {
     cache_line_size(2, CacheType::Unified)
 }
 
+/// Returns the total size in bytes of the unified L3 cache.
+///
+/// The only possibilities for this returning `None` are if the system does not support cache
+/// parameters, in which case [`get_cache_parameters()`](raw_cpuid::CpuId::get_cache_parameters) will
+/// fail, or if the system has no unified L3 cache.
+///
+/// This is computed as `associativity * line_size * sets`, and if there are multiple caches
+/// available, it returns the size of the **smallest** cache.
 #[inline]
 pub fn l3_cache_size() -> Option<usize> {
     cache_size(3, CacheType::Unified)
 }
 
+/// Returns the line size in bytes of the unified L3 cache.
+///
+/// The only possibilities for this returning `None` are if the system does not support cache
+/// parameters, in which case [`get_cache_parameters()`](raw_cpuid::CpuId::get_cache_parameters) will
+/// fail, or if the system has no unified L3 cache.
+///
+/// This is computed from [`coherency_line_size()`](raw_cpuid::CacheParameter::coherency_line_size),
+/// and if there are multiple caches available, it returns the size of the **smallest** cache.
 #[inline]
 pub fn l3_cache_line_size() -> Option<usize> {
     cache_line_size(3, CacheType::Unified)
