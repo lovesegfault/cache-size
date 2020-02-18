0096d4871229bc52359d32d5dd61bde0ed153ff8
Bernardo Meurer <meurerbernardo@gmail.com>
2020-02-18 13:04:55 -0800

    Revert "blanket: add warnings"
    This reverts commit 952d4d4e92e04257c30d8db021c4addfb7c31f9c.

--- src/blanket.rs
+++ src/blanket.rs
@@ -1,16 +1,10 @@
 use crate::CacheType;
 
-#[deprecated(
-    note = "You are using a blanket implementation of cache-size because your architecture is not supported."
-)]
 #[inline]
 fn cache_size(_level: u8, _cache_type: CacheType) -> Option<usize> {
     None
 }
 
-#[deprecated(
-    note = "You are using a blanket implementation of cache-size because your architecture is not supported."
-)]
 #[inline]
 fn cache_line_size(_level: u8, _cache_type: CacheType) -> Option<usize> {
     None
