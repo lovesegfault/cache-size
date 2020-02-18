3c66c2c6b3f7fbd3f4331b105fc8561e5f77570b
Bernardo Meurer <meurerbernardo@gmail.com>
2020-02-18 11:17:42 -0800

    shell: init

---
+++ shell.nix
@@ -0,0 +1,24 @@
+let
+  moz_overlay = import (
+    builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz
+  );
+  nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+  stable = nixpkgs.rustChannelOf { channel = "stable"; };
+  extensions = [
+      "clippy-preview"
+      "rls-preview"
+      "rustfmt-preview"
+      "rust-analysis"
+      "rust-std"
+      "rust-src"
+  ];
+  rustStable = stable.rust.override { extensions = extensions; };
+in
+  with nixpkgs;
+  mkShell {
+    name = "chirp";
+    buildInputs = [
+      cargo-edit
+      rustStable
+    ];
+  }
